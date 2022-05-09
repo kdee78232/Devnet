c26bdc269733467133a172dc46c10254544d94cb
kdee78232 <kdee78232@GitHub.com>
2022-05-08 21:04:44 -0400

    new files curl demo

--- testing/error_handler.py
+++ testing/error_handler.py
@@ -1,20 +1,5 @@
-x=0
-while True:
-    try:
-        filename= input('Which file do you want to open?' ':' '\n')
-        with open(filename, "r")as data:
-            file_data=data.read()
-    except FileNotFoundError:
-        print(f"Sorry, {filename} doesn't exist! Please try again")
+def cars(**kwargs):
+    for key, value in kwargs.items():
+        print("hello", value)
 
-    else:
-        print(file_data)
-        x=0
-        break
-    finally:
-        x+=1
-        if x==3:
-            print("you are at limit")
-            break
-
-
+cars(kwarg1="john",kwarg2='sue')
